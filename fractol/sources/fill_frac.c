6305ee7c4900177bd1b414ede02693d0fbd6088b
Ryuu Leonardo Sato <leonarudoryuu0629@gmail.com>
2025-07-13 19:35:06 +0900

    Add files via upload

--- fractol/sources/fill_frac.c
+++ fractol/sources/fill_frac.c
@@ -6,7 +6,7 @@
 /*   By: rysato <rysato@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/14 20:24:41 by rysato            #+#    #+#             */
-/*   Updated: 2025/07/13 19:22:09 by rysato           ###   ########.fr       */
+/*   Updated: 2025/07/13 19:28:43 by rysato           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,13 +57,13 @@ void	initialize_fra(int argc, char **argv, t_frac *fra)
 		fra->ju_real = ft_atod(argv[2]); //実部
 		fra->ju_imag = ft_atod(argv[3]); //虚部
 	}
-	fra->mlx = mlx_init();
+	fra->mlx = mlx_init();//x server
 	if (fra->mlx == NULL)
 		exit(1);
-	fra->win = mlx_new_window(fra->mlx, WIN_W, WIN_H, "fract-ol");
+	fra->win = mlx_new_window(fra->mlx, WIN_W, WIN_H, "fract-ol");//window pointer
 	if (fra->win == NULL)
 		exit(1);
-	fra->img.img_ptr = mlx_new_image(fra->mlx, WIN_W, WIN_H);
+	fra->img.img_ptr = mlx_new_image(fra->mlx, WIN_W, WIN_H);//裏で一枚作成
 	fra->img.data = mlx_get_data_addr(fra->img.img_ptr, &fra->img.bit_p_pix,
 			&fra->img.byte_p_law, &fra->img.endian);
 	fra->view.max_imag = 2.0;
